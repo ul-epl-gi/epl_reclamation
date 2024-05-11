ca74433f1f7a92a5f100f4312562c0a1a8a9ac07
Noyoule <victoirenoyouliwa@gmail.com>
2024-05-11 23:41:49 +0000

    First commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use  Telegram\Bot\Laravel\Facades\Telegram;
+
+Route::get('/', function () {
+    // $response = Telegram::bot('mybot')->getMe();
+   // $response = Telegram::setMyCommands(['commands' => [['command' => 'start', 'description' => 'start']]]);
+});
